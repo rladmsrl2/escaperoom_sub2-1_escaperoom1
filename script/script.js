dc522b76ad5d93131b0a71df7935bc944074fcb0
rladmsrl2 <96101705+rladmsrl2@users.noreply.github.com>
2021-12-24 16:07:05 +0900

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,34 @@
+$(window).scroll(function() {
+    var height = $(window).scrollTop();
+    if (height > 100) {
+        $('.icon_top').fadeIn();
+    } else {
+        $('.icon_top').fadeOut();
+    }
+});
+
+/*이코디언*/
+$(function(){
+	$(".box1-1> ul> li").click(function(){
+		$(this).children("div").slideToggle();
+		$(this).toggleClass('on').siblings().removeClass('on');
+		$(this).siblings().children("div").slideUp();
+        $(".box1-2> ul> li").children("div").slideUp();
+        $(".box1-2> ul> li").removeClass('on');
+        $(".icon_top").children("div").slideUp();
+	});
+    $(".box1-2> ul> li").click(function(){
+		$(this).children("div").slideToggle();
+		$(this).toggleClass('on').siblings().removeClass('on');
+		$(this).siblings().children("div").slideUp();
+        $(".box1-1> ul> li").children("div").slideUp();
+        $(".box1-1> ul> li").removeClass('on');
+        $(".icon_top").children("div").slideUp();
+	});
+    $(".icon_top").click(function(){
+        $(".box1-1> ul> li").children("div").slideUp();
+        $(".box1-2> ul> li").children("div").slideUp();
+        $(".box1-1> ul> li").removeClass('on');
+        $(".box1-2> ul> li").removeClass('on');
+	});
+});
